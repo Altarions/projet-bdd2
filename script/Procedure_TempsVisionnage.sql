4b412b1670686b4e382e9e26d9ead92c0f2dbbc7
Rom123Soleil <romain.dupont@etu.univ-nantes.fr>
2021-03-12 08:03:24 +0100

    Réparation de la procédure

--- script/Procedure_TempsVisionnage.sql
+++ script/Procedure_TempsVisionnage.sql
@@ -1,21 +1,17 @@
 set serveroutput on format wrapped;
 
-CREATE OR REPLACE PROCEDURE TempsVisionnagePourCompte(newMail IN VARCHAR2) is
+CREATE OR REPLACE PROCEDURE TempsVisionnagePourCompte(newMail IN VARCHAR) is
     actuel NUMBER :=0;
     nombretotal NUMBER :=0;
     Cursor cursor1 is
     SELECT TEMPS_VISIONNAGE
     FROM VISIONNAGE
-    WHERE VISIONNAGE.MAIL = newMail;
-
+    WHERE MAIL = newMail;
 BEGIN
-      open Cursor1;
-     IF (cursor1%ROWCOUNT =0)
-                 THEN
-         RAISE_APPLICATION_ERROR(-20001, 'Le compte nexiste pas voyons');
-     end if;
+     open Cursor1;
       loop
         fetch cursor1 into actuel;
+        DBMS_OUTPUT.put_line(actuel);
         exit when cursor1%NOTFOUND;
         nombretotal := nombretotal + actuel;
       end loop;
